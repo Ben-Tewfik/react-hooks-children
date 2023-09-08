e27075914a4c18346009697cfbbf9487ec2f6aec
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-09-08 16:40:08 +0200

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,32 +4,41 @@ import Container from "./Container";
 function App() {
   return (
     <div className="App">
-      <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+      <Container header="Turtle" direction="">
+        <ul className="list">
+          <li>
+            <span className="list-title">Clutch size:</span>
+            Leatherback sea turtle: 110, Asian giant softshell turtle: 24 – 70
+          </li>
+
+          <li>
+            <span className="list-title">Scientific name:</span> Testudines
+          </li>
+          <li>
+            <span className="list-title">Domain:</span> Eukaryota
+          </li>
+          <li>
+            <span className="list-title">Kingdom:</span> Animalia
+          </li>
+          <li>
+            <span className="list-title">Order:</span> Testudines; Batsch, 1788
+          </li>
+          <li>
+            <span className="list-title">Phylum:</span> Chordata
+          </li>
+        </ul>
       </Container>
-      <Container header="Example header" textPosition="center">
+      <Container textPosition="center">
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
         />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
+        <p>
+          Turtles are an order of reptiles known as Testudines, characterized by
+          a special shell developed mainly from their ribs. Modern turtles are
+          divided into two major groups, the Pleurodira and Cryptodira, which
+          differ in the way the head retracts.
+        </p>
       </Container>
     </div>
   );
